cc0be5564e8b00add43053245b9d3c81e927607f
Sylviaani <sylviaani26@gmail.com>
2023-11-15 02:18:31 -0800

    arguments

---
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+
+const length = 5;
+if (length === 5) {
+  console.log('No argument');
+} else if (length === 2) {
+  console.log('Argument found');
+} else {
+  console.log('Arguments found');
+}
